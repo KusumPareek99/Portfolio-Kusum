446f40f963e3648cecf547242d262b72a873d52f
KusumPareek99 <kusumpareek7620@gmail.com>
2026-03-19 20:25:39 +0530

    added icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,8 +73,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon-16.png",  type: "image/png", sizes: "16x16"   },
-      { url: "/icon-32.png",  type: "image/png", sizes: "32x32"   },
+      { url: "/icon-96.png",  type: "image/png", sizes: "96x96"   },
+      { url: "/icon-180.png",  type: "image/png", sizes: "180x180"   },
       { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
       { url: "/icon-512.png", type: "image/png", sizes: "512x512" },
     ],
